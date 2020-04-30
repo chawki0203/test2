50bf9215de4cd1f15e38792589a4999246acdd35
Glitch (webd236-homework-starter) <none>
2020-04-30 00:36:01 +0000

    👘👀 Checkpoint ./view.php:86128/1699

--- view.php
+++ view.php
@@ -6,12 +6,34 @@
 
   include 'include/header.html';
 
-  STU_NUM = safeParam($_REQUEST, "STU_NUM", "");
-  if (!STU_NUM) {
-    die("no such student found")
+  $stu_num = safeParam($_REQUEST, "STU_NUM", "");
+  if (!$stu_num) {
+    die("no student number given");
   }
-?>
 
+  $student = findStudentById($stu_num);
+  $advisor = findEmployeeById($student['EMP_NUM']);
+
+  $fields = array (
+    "STU_FNAME" => "First name",
+    "STU_LNAME" => "Last name",
+    "STU_INITIAL" => "Middle initial",
+    "STU_DOB" => "Date of birth",
+    "STU_HRS" => "Hours",
+    "STU_CLASS" => "Class",
+    "DEPT_CODE" => "Department code",
+    "STU_PHONE" => "Phone number",
+  );
+
+?>
+      <div class="row">
+        <div class="col-lg-8 offset-2">
+          <form action="index.php" method="post">
+            
+          </form>
+          <p>Advisor: <?php echo "{$advisor['EMP_LNAME']}, {$advisor['EMP_FNAME']}"; ?></p>
+        </div>
+      </div>
 <?php
   include 'include/footer.html';
 ?>
